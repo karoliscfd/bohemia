cb5a10a056ac7df6b6a1e199bd9b7c072e53e966
benmbrew <benmbrew@gmail.com>
2021-03-01 15:30:52 -0500

    now the function takes the path of both signatures and overlays them on the hh certificate. the signatrues are stored in the signature folder and ignored

--- rpackage/bohemia/R/hh_qr_code_print.R
+++ rpackage/bohemia/R/hh_qr_code_print.R
@@ -3,6 +3,8 @@
 #' Overlay qr code on image
 #' @param base_img_path The path to the image that will be overlayed with a qr code
 #' @param hh_id The household ID
+#' @param demog_sig The path to the demography signature file
+#' @param pi_sig The path to the PI signature file
 #' @param save_file Where to save the file
 #' @import magick
 #' @import qrcode
@@ -10,8 +12,7 @@
 #' @return overlayed image
 #' @export
 
-
-hh_qr_code_print <- function(base_img_path, hh_id, save_file = NULL, ...) {
+hh_qr_code_print <- function(base_img_path, hh_id, demo_sig, pi_sig ,save_file = NULL, ...) {
   # create qr code matrix
   x <- qrcode_gen(hh_id, plotQRcode=F, dataOutput=T, ErrorCorrectionLevel = 'H')
   x <- as.data.frame(x)
@@ -34,8 +35,25 @@ hh_qr_code_print <- function(base_img_path, hh_id, save_file = NULL, ...) {
   base_img <- image_scale(base_img, geometry = "50%x")
   base_img <- image_annotate(base_img, hh_id, size = 30, gravity = "center", color = "black", location='+10+25')
   
+  # read in signature images
+  demo_img <- image_read(demo_sig)
+  pi_img <- image_read(pi_sig)
+  demo_img <- image_scale(demo_img, geometry = "50%x")
+  pi_img <- image_scale(pi_img, geometry = "50%x")
+  demo_img <- image_crop(demo_img, "200x40" )
+  
+  # overlay signatures
+  img_with_sig <- base_img %>% image_composite(gravity = 'southwest',
+    demo_img,
+    offset = "+250+130"
+  )
+  img_with_sig <- img_with_sig %>% image_composite(gravity = 'southeast',
+                                               pi_img,
+                                               offset = "+250+130"
+  )
+  
   # layer images
-  img_with_inset <- base_img %>% image_composite(
+  img_with_inset <- img_with_sig %>% image_composite(
     img_inset,
     offset = "+420+40"
   )
