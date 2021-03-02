78f7e1d932c4fb4170fb2ff18b2ea8ddbe842e7e
linl33 <linlee1997@gmail.com>
2021-03-02 17:57:56 +0800

    Improve form table of contents

--- odkx/app/config/assets/customPromptTypes/customToc.js
+++ odkx/app/config/assets/customPromptTypes/customToc.js
@@ -1,57 +1,32 @@
 'use strict';
 
-define(['promptTypes', 'handlebarsHelpers', 'handlebars', 'jquery', 'prompts'], function(promptTypes, handlebarsHelpers, Handlebars, $) {
+define(['promptTypes', 'handlebars', 'underscore', 'jquery', 'prompts'], function(promptTypes, Handlebars, _, $) {
   Handlebars.registerHelper('localizeTextSafe', function (displayObjectField) {
     return new Handlebars.SafeString(Handlebars.helpers['localizeText'](displayObjectField));
-  })
+  });
 
   return {
     contents: promptTypes.contents.extend({
-      templatePath: '../config/assets/customPromptTypes/templates/contents.handlebars'/*,
-      configureRenderContext: function (ctxt) {
-        promptTypes.contents.prototype.configureRenderContext(this, $.extend({}, ctxt, {
-          success: function () {
+      templatePath: '../config/assets/customPromptTypes/templates/contents.handlebars',
+      configureRenderContext: function(ctxt) {
+        var that = this;
 
+        promptTypes.contents.prototype.configureRenderContext.apply(this, [$.extend({}, ctxt, {
+          success: function () {
+            that.renderContext.prompts = _.groupBy(that.renderContext.prompts, function (prompt) {
+              return prompt.display.section;
+            });
+
+            delete that.renderContext.prompts['undefined'];
+
+            ctxt.success();
           }
-        }))
-      }*/
+        })]);
+      },
+      afterRender: function () {
+        // remove empty sections
+        $('.panel:has(.list-group:empty)').remove();
+      }
     })
   }
-  // return {
-  //   linked_table_counting: promptTypes.linked_table.extend({
-  //     configureRenderContext: function (ctxt) {
-  //       var that = this;
-  //       var queryDefn = opendatakit.getQueriesDefinition(this.values_list);
-  //
-  //       var modifiedCtxt = $.extend({}, ctxt, {
-  //         success: function () {
-  //           var subFormStatusCol = queryDefn.subFormStatusCol;
-  //
-  //           if (subFormStatusCol === undefined || subFormStatusCol === null || subFormStatusCol === '') {
-  //             that.setValueDeferredChange(countInstances(that.renderContext.instances));
-  //             ctxt.success();
-  //           } else {
-  //             var modifiedSel = that._cachedSelection || '';
-  //             if (modifiedSel !== '') {
-  //               modifiedSel += ' AND ';
-  //             }
-  //             modifiedSel += '"' + queryDefn.subFormStatusCol + '" = ?';
-  //
-  //             var modifiedSelArgs = queryDefn.selectionArgs() || [];
-  //             modifiedSelArgs.push('1');
-  //
-  //             database.get_linked_instances($.extend({}, ctxt, {
-  //               success: function (instanceList) {
-  //                 that.setValueDeferredChange(countInstances(instanceList));
-  //                 ctxt.success();
-  //               }
-  //             }), that.getLinkedTableId(), modifiedSel, modifiedSelArgs);
-  //           }
-  //         }
-  //       });
-  //
-  //       promptTypes.linked_table.prototype.configureRenderContext.apply(this, [modifiedCtxt]);
-  //     }
-  //   })
-  // }
 });
--- odkx/app/config/tables/census/forms/census/customPromptTypes.js
+++ odkx/app/config/tables/census/forms/census/customPromptTypes.js
@@ -4,12 +4,13 @@ define([
   '../../../../assets/customPromptTypes/linked_table_counting',
   '../../../../assets/customPromptTypes/next_extid',
   '../../../../assets/customPromptTypes/customToc',
+  '../../../../assets/customPromptTypes/customNote',
   'promptTypes',
   'controller',
   'jquery',
   '../../../../assets/customPromptTypes/hamlet_info'
-], function(linked_table_counting, next_extid, customToc, promptTypes, controller, $) {
-  return Object.assign({}, linked_table_counting, next_extid, customToc, {
+], function(linked_table_counting, next_extid, customToc, customNote, promptTypes, controller, $) {
+  return Object.assign({}, linked_table_counting, next_extid, customToc, customNote, {
     exit_survey: promptTypes.base.extend({
       type: 'exit_survey',
       template: function () { return ''; },
