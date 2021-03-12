a8443ca7951054db01c1ff2e4a615bf362933cdf
Joe Brew <joebrew@gmail.com>
2021-03-12 11:57:26 +0100

    updated

--- analyses/interVA5/convert_data.R
+++ analyses/interVA5/convert_data.R
@@ -3,17 +3,29 @@ library(openVA)
 library(nbc4va)
 library(InterVA5)
 library(bohemia)
+library(RPostgres)
+is_local <- FALSE
+drv <- RPostgres::Postgres()
+# read in credenstials 
+creds <- yaml::yaml.load_file('../../credentials/credentials.yaml')
 
-# mozambique
-moz_forms <- load_odk_data(the_country = 'Mozambique',
-                           credentials_path = '../../credentials/credentials.yaml',
-                           users_path = '../../credentials/users.yaml',
-                           local = FALSE, 
-                           efficient=FALSE,
-                           use_cached = FALSE)
-va <- moz_forms$va
 
-# save.image('temp.RData')
+if(is_local){
+  con <- dbConnect(drv, dbname='bohemia')
+} else {
+  psql_end_point = creds$endpoint
+  psql_user = creds$psql_master_username
+  psql_pass = creds$psql_master_password
+  con <- dbConnect(drv, dbname='bohemia', host=psql_end_point, 
+                   port=5432,
+                   user=psql_user, password=psql_pass)
+}
+
+va <- dbGetQuery(conn = con,
+                 'SELECT * FROM va LIMIT 100')
+
+dbDisconnect(con)
+
 # CrossVA package example data (VA version 1.5.1)
 datafile_2016_151 <- system.file("sample", "who151_odk_export.csv", package = "CrossVA")
 records_2016_151 <- read.csv(datafile_2016_151)
@@ -23,7 +35,8 @@ out2 <- InterVA5(whoData2016_151, HIV = "l", Malaria = "l", directory = getwd())
 summary(out2)
 
 # our data (VA version 1.5.3)
-dat <- read.csv('2016_WHO_Verbal_Autopsy_Form_1_5_3_results.csv')
+dat <- va
+dat <- read.csv('2016_WHO_Verbal_Autopsy_Form_1_5_3_results.csv') # https://trello.com/c/ZiqTjBeD/2328-bohemia-get-our-data-into-interva-5-format
 # right now only one row, so create a fake 2nd row
 dat <- rbind(dat, dat)
 temp <- odk2openVA_v151(dat)
