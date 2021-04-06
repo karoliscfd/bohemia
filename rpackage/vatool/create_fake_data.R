cb75cfa506bba3294a2aa03b54fd4dbd8d3a6d91
benmbrew <benmbrew@gmail.com>
2021-04-06 11:23:00 -0400

    added other cods to app

--- rpackage/vatool/create_fake_data.R
+++ rpackage/vatool/create_fake_data.R
@@ -18,8 +18,12 @@ users <-
 cods <- 
   tibble(user_id = c(1,2,1,2,3,4,5,3),
          death_id= c('MTM-013-701', 'MTM-013-701', 'NAU-040-701','MBI-076-701', 'KUK-129-701', 'KUK-129-701','KAI-013-701', 'KAI-013-701' ),
-         cod_code = c('G03', 'P23', 'G03', 'A06', 'A09', 'A15', 'A20', 'A20'),
-         cod = c('Meningitis', 'Pneumonia', 'Meningitis','Dysentery Acute/Chronic', 'Diarrhoea','TB Confirmed', 'Plague','Plague'),
+         cod_code_1 = c('G03', 'P23', 'G03', 'A06', 'A09', 'A15', 'A20', 'A20'),
+         cod_1 = c('Meningitis', 'Pneumonia', 'Meningitis','Dysentery Acute/Chronic', 'Diarrhoea','TB Confirmed', 'Plague','Plague'),
+         cod_code_2 = c('A06', 'D48', 'O71', 'P95', 'T71',  'A20','A15' ,'Y08'),
+         cod_2 = c('Dysentery Acute/Chronic', 'Tumours Other non-malignant', 'Rupture uterus','Stillbirth (macerated)', 'Asphyxiation', 'Plague','TB Confirmed','Assault by other specified means'),
+         cod_code_3 = c('T67', 'G03', 'A06', 'A09', 'A15', 'A20', 'A20', 'P23'),
+         cod_3 = c('Effects of heat and light', 'Meningitis','Dysentery Acute/Chronic', 'Diarrhoea','TB Confirmed', 'Plague','Plague', 'Pneumonia'),
          time_stamp = c("2021-02-16 09:46:39 EDT", "2021-01-11 08:46:39 EDT", "2021-01-16 11:36:39 EDT", "2021-03-15 09:46:39 EDT", "2021-03-08 09:50:39 EDT", "2021-02-16 09:46:39 EDT", "2021-03-16 09:45:39 EDT", "2021-03-16 09:46:39 EDT"))
 
 # Define connection
