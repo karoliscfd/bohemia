d8029527ef1e95f54671cb77d1a19956f3b015e4
Joe Brew <joebrew@gmail.com>
2021-04-11 14:17:16 +0200

    icf names

--- rpackage/icf/R/mod_error_check.R
+++ rpackage/icf/R/mod_error_check.R
@@ -6,7 +6,11 @@ mod_error_check_ui <- function(id, question = 'Some rando') {
   fluidRow(
     column(4, 
            checkboxInput(ns('question1'), question, value = FALSE)),
-    column(8,
+    column(4,
+           radioButtons(ns('questionx'), label = 'Check the ICF, has the error been corrected in the field by the field worker?', 
+                        choices = c('Yes', 'No'),
+                        selected = character(length = 0)))
+    column(4,
            uiOutput(ns('q2')))
   )
 }
@@ -15,16 +19,16 @@ mod_error_check <- function(input, output, session) {
 
   reactive_val <- reactiveVal(value = FALSE)
 
-  output$q2 <- renderUI({
-    x <- input$question1
-    if(!is.null(x)){
-      if(x){
-        radioButtons('question2', label = 'Check the ICF, has the error been corrected in the field by the field worker?', 
-                     choices = c('Yes', 'No'),
-                     selected = character(length = 0))
-      }
-    }
-  })
+  # output$q2 <- renderUI({
+  #   x <- input$question1
+  #   if(!is.null(x)){
+  #     if(x){
+  #       radioButtons('question2', label = 'Check the ICF, has the error been corrected in the field by the field worker?', 
+  #                    choices = c('Yes', 'No'),
+  #                    selected = character(length = 0))
+  #     }
+  #   }
+  # })
 
   ro <- reactive({
     input$question1
