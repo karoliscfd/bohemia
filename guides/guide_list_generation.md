edda6cfe1a8b8ce1a53cfcbc879b34e76181213b
Joe Brew <joebrew@gmail.com>
2021-04-27 09:54:14 +0200

    visit control list generation

---
+++ guides/guide_list_generation.md
@@ -0,0 +1,33 @@
+# List generation
+
+## Context 
+
+For reasons of operational guidance, internal accountability, and data capture efficiency, Bohemia field sites require the ability to automatically generate "lists". These lists are generated locally by data managers so as to include names data. This document contains instructions for generating those lists.
+
+
+## Prerequisites
+
+In order to generate lists, you need a private and public key on your system, as per the [schema for handling names data](https://github.com/databrew/bohemia/blob/master/misc/handling_names.md). These files should be named as follows:
+
+- `bohemia_pub.pem`
+- `bohemia_priv.pem`
+
+It is important that you never share these files with anyone, nor put them in any location where they might be accessed by someone other than you. If you do not have these files on your system, please contact bohemia@databrew.cc.
+
+## Visit control sheet
+
+To generate the visit control sheet, run the following code:
+
+```
+priv_key <- 'bohemia_priv.pem' # change to path to your local file
+pub_key <- 'bohemia_pub.pem' # change to path to your local file
+
+list_generation_visit_control(keyfile = priv_key,
+                              keyfile_public = pub_key,
+                              location = 'CUD',
+                              output_file = NULL)
+```
+
+Note that the `location` argument can be set to the 3 letter hamlet code for one location. Alternatively, you can set to `NULL` (in which case ALL locations will be rendered) or to multiple locations by passing a character vector of length >1 (for example, `c('KLD, 'DIM', 'KRE', 'NWE')`).
+
+If the `output_file` argument is left as `NULL` (the default), the function will return an in-memory dataframe for further manipulation. Alternatively, pass a path to any not yet existent file with a `.csv` extension, and the function will instead write a csv locally.
---
+++ rpackage/bohemia/R/list_generation_visit_control.R
@@ -0,0 +1,72 @@
+#' Visit control sheet list generation
+#' 
+#' Render a visit control sheet
+#' @param keyfile The path to the private key
+#' @param keyfile_public The path to the public key
+#' @param location A three letter location code (or character vector of multiple codes). If NULL, all locations to be used
+#' @param output_file The name of the spreadsheet to be written (csv file); if null, will return the table in memory
+#' @import dplyr
+#' @import PKI
+#' @import readr
+#' @return A table will be created
+#' @export
+
+list_generation_visit_control <- function(keyfile = NULL,
+                                          keyfile_public = NULL,
+                                          location = NULL,
+                                          output_file = NULL){
+  
+  # # REMOVE THE BELOW
+  # keyfile <- '../../../credentials/bohemia_priv.pem'
+  # keyfile_public <- '../../../credentials/bohemia_pub.pem'
+  # library(dplyr)
+  # library(PKI)
+  # library(bohemia)
+  
+  # Deal with keyfile
+  if(is.null(keyfile)){
+    stop('You must specify the path to a private key (pem file) for decrypting names.')
+  }
+  if(is.null(keyfile_public)){
+    stop('You must specify the path to a public key (pem file) for encrypting names.')
+  }
+  
+  # Deal with location
+  locs <- bohemia::locations
+  if(!is.null(location)){
+    locs <- locs %>% filter(code %in% location)
+  }
+  if(nrow(locs) < 1){
+    stop('No data found for the specified locations.')
+  }
+  
+  ################################# Create dummy data #########################################
+  # Encrypt the names
+  the_names <- c('John Doe', 'Jane Doe', 'Tarzan Jungle', 'Bob Smith', 'Alice Wonderland')
+  the_names_encrypted <- encrypt_private_data(data = the_names, keyfile = keyfile_public)
+  # Decrypt the names
+  the_names <- decrypt_private_data(data = the_names_encrypted, keyfile = keyfile)
+  
+  # Create some household IDs for the geographies in question
+  indices <- sample(1:nrow(locs), length(the_names), replace = TRUE)
+  codes <- locs$code[indices]
+  hhids <- paste0(codes, '-', sample(c('123','234', '345', '456'), length(the_names), replace = TRUE))  
+  df <- locs[indices,] %>% dplyr::select(District, Ward, Village, Hamlet)
+  df$hhid <- hhids
+  df$hh_head_name <- sample(the_names, nrow(df), replace = TRUE)
+  df$hh_sub_name <- sample(the_names, nrow(df), replace = TRUE)  
+  get_contact <- function(){ paste0(sample(0:9, 8, replace = T), collapse = '')}
+  make_contact <- function(n){out <- c(); for(i in 1:n){out[i] <- get_contact()};return(out)}
+  df$contact_information <- make_contact(n = nrow(df))
+  df$previous_attempts <- 0
+  df <- df %>% arrange(hhid) %>%
+    dplyr::select(hhid, hh_head_name, hh_sub_name,
+                  District, Ward, Village, Hamlet, contact_information, previous_attempts)
+  
+  if(!is.null(output_file)){
+    message('Writing a csv to ', output_file)
+    write_csv(df, output_file)
+  } else {
+    return(df)
+  }
+}
