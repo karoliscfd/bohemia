a5c50ab943096251c06d91173c219ba4b4bd6912
Joe Brew <joebrew@gmail.com>
2021-05-11 19:22:31 +0200

    ento

--- scripts/ento_excel_to_xml.py
+++ scripts/ento_excel_to_xml.py
@@ -3,6 +3,8 @@ import os
 import shutil
 os.chdir('../credentials')
 
+## ENTO A1
+
 s = ezsheets.Spreadsheet('https://docs.google.com/spreadsheets/d/1EiYNYG0FnLFFw35JUrPOhjzB9bLQOAxYKu3jY_zKXOk/edit#gid=0')
 s.downloadAsExcel()
 
@@ -16,3 +18,19 @@ shutil.move('entoa1.xlsx', '../forms/entoa1/entoa1.xlsx')
 shutil.move('entoa1.xml',  '../forms/entoa1/entoa1.xml')
 
 print('Done. Docs in forms/entoa1.')
+
+## ENTO A2
+
+s = ezsheets.Spreadsheet('https://docs.google.com/spreadsheets/d/1Sg438_a49yE1ps1IfN0aW2ZC0p0i-jrw1YRQ20Ppb2Q/edit#gid=0')
+s.downloadAsExcel()
+
+## Convert to xml
+os.system('xls2xform entoa2.xlsx entoa2.xml ')
+
+# Move
+if not os.path.isdir('../forms/entoa2/'):
+    os.mkdir('../forms/entoa2')
+shutil.move('entoa2.xlsx', '../forms/entoa2/entoa2.xlsx')
+shutil.move('entoa2.xml',  '../forms/entoa2/entoa2.xml')
+
+print('Done. Docs in forms/entoa2.')
