95b6d93bbf5a71dc62ada3402166a3b98410c542
Jeff Beorse <jbeorse@Jeffs-MacBook-Pro.local>
2021-05-19 21:45:34 -0700

    Add sequenced confirmation to hh flow

--- odkx/app/config/tables/census/js/census_list.js
+++ odkx/app/config/tables/census/js/census_list.js
@@ -91,20 +91,29 @@
     document.getElementById('hhRosterModalTitle').textContent = hhId;
     document.getElementById('hhRosterModalBody').textContent = '';
     document.getElementById('rosterConfirm3Div').classList.add('d-none');
-    document.getElementById('rosterConfirm3').checked = false;
+    document.getElementById('rosterConfirm3').classList.remove('is-invalid');
 
     var rosterConfirm1 = document.getElementById('rosterConfirm1');
     var rosterConfirm2 = document.getElementById('rosterConfirm2');
+    rosterConfirm2.classList.add('d-none');
 
-    rosterConfirm1.checked = true;
-    rosterConfirm2.checked = true;
+    if (!!rosterConfirm1.querySelector('.active')) {
+      rosterConfirm1.querySelector('.active').classList.remove('active');
+    }
+    if (!!rosterConfirm2.querySelector('.active')) {
+      rosterConfirm2.querySelector('.active').classList.remove('active');
+    }
+
+    rosterConfirm1.addEventListener('change', function () {
+      rosterConfirm2.classList.remove('d-none');
+    });
 
     if (hhMinicenced && isFwInMoz()) {
-      rosterConfirm1.addEventListener('change', matchCheckboxListener);
-      rosterConfirm2.addEventListener('change', matchCheckboxListener);
+      rosterConfirm1.addEventListener('change', matchToggleListener);
+      rosterConfirm2.addEventListener('change', matchToggleListener);
     } else {
-      rosterConfirm1.removeEventListener('change', matchCheckboxListener);
-      rosterConfirm2.removeEventListener('change', matchCheckboxListener);
+      rosterConfirm1.removeEventListener('change', matchToggleListener);
+      rosterConfirm2.removeEventListener('change', matchToggleListener);
     }
 
     odkData.arbitraryQuery(
@@ -140,8 +149,8 @@
     var currTarget = evt.currentTarget;
     var hhMinicensed = currTarget.dataset['hhMinicenced'] === 'yes';
 
-    var rosterMatch = document.getElementById('rosterConfirm1').checked;
-    var hhIdMatch = document.getElementById('rosterConfirm2').checked;
+    var rosterMatch = selectedRosterMatch();
+    var hhIdMatch = selectedHhIdMatch();
     var completeMatch = (rosterMatch && hhIdMatch) || currTarget.id === 'hhRosterModalProceed';
 
     if (completeMatch) {
@@ -150,6 +159,7 @@
       if (currTarget.id === 'hhRosterModalContinue' && isFwInMoz() && hhMinicensed && !rosterMatch && hhIdMatch) {
         // this requires confirmation to delete data
         if (!document.getElementById('rosterConfirm3').checked) {
+          document.getElementById('rosterConfirm3').classList.add('is-invalid');
           return;
         }
 
@@ -218,16 +228,31 @@
     }
   };
 
-  var matchCheckboxListener = function () {
-    var rosterMatch = document.getElementById('rosterConfirm1').checked;
-    var hhIdMatch = document.getElementById('rosterConfirm2').checked;
+  var matchToggleListener = function () {
+    var confirm1Selected = document.querySelector('#rosterConfirm1 .active');
+    var confirm2Selected = document.querySelector('#rosterConfirm2 .active');
 
-    if (!rosterMatch && hhIdMatch) {
-      document.getElementById('rosterConfirm3Div').classList.remove('d-none');
-    } else {
-      document.getElementById('rosterConfirm3Div').classList.add('d-none');
+    if (confirm1Selected && confirm2Selected) {
+      var rosterMatch = selectedRosterMatch();
+      var hhIdMatch = selectedHhIdMatch();
+
+      if (!rosterMatch && hhIdMatch) {
+        document.getElementById('rosterConfirm3Div').classList.remove('d-none');
+      } else {
+        document.getElementById('rosterConfirm3Div').classList.add('d-none');
+      }
     }
   };
+
+  var selectedRosterMatch = function () {
+    var selected = document.querySelector('#rosterConfirm1 .active');
+    return selected && selected.id === 'rosterConfirm1Yes'
+  }
+
+  var selectedHhIdMatch = function () {
+    var selected = document.querySelector('#rosterConfirm2 .active');
+    return selected && selected.id === 'rosterConfirm2Yes'
+  }
 
   var hhRosterConfirm = function (evt) {
     // modified from odkTables.editRowWithSurvey
